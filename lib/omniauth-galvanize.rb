b184ededc5d52d341ed906822bf11cb596e5d84f
Christian Cunningham <ccunningham@Chriss-MacBook-Pro.local>
2015-10-23 10:16:26 -0600

    Get rid of unnecessary strategies

--- lib/omniauth-galvanize.rb
+++ lib/omniauth-galvanize.rb
@@ -1,5 +1,2 @@
-require "omniauth-galvanize/version"
-require 'omniauth/strategies/galvanize_developer'
-require 'omniauth/strategies/galvanize_local'
-require 'omniauth/strategies/galvanize_stage'
+require 'omniauth-galvanize/version'
 require 'omniauth/strategies/galvanize'
