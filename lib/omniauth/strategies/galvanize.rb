df1afed2a0da464d8fcf8c848c9ecdc7d76a44ea
jejacks0n <jejacks0n@gmail.com>
2016-10-27 21:10:07 -0600

    formats the galvanize strategy.
    - normalizes info to match quilt more
    - deprecates the concept of `cohort` (singular)

--- lib/omniauth/strategies/galvanize.rb
+++ lib/omniauth/strategies/galvanize.rb
@@ -3,49 +3,52 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Galvanize < OmniAuth::Strategies::OAuth2
+
       option :name, 'galvanize'
-
       option :client_options, {
-                                :site => 'https://members.galvanize.com',
-                                :authorize_url => '/accounts/authorize',
-                                :token_url => '/accounts/token'
-                            }
+        site: 'https://members.galvanize.com',
+        authorize_url: '/accounts/authorize',
+        token_url: '/accounts/token'
+      }
 
-      SETUP_PROC = lambda do |env|
+      OmniAuth.config.before_request_phase = lambda do |env|
         if env['omniauth.strategy'].options[:sign_up]
           env['omniauth.strategy'].options[:client_options][:authorize_url] = 'accounts/signup'
         end
       end
 
-      OmniAuth.config.before_request_phase = SETUP_PROC
-
-      uid{ raw_info['results'][0]['id'] }
+      uid do
+        raw_result['id']
+      end
 
       info do
         {
-            :email => raw_info['results'][0]['email'],
-            :name => raw_info['results'][0]['name'],
-            :first_name => raw_info['results'][0]['first_name'],
-            :last_name => raw_info['results'][0]['last_name'],
-            :galvanize_id => raw_info['results'][0]['id'],
-            :about => raw_info['results'][0]['about'],
-            :photo => raw_info['results'][0]['photo'],
-            :roles => raw_info['results'][0]['roles'],
-            :home_location => raw_info['results'][0]['home_location'],
-            :cohort => raw_info['results'][0]['cohorts'],
-            :companies => raw_info['results'][0]['companies'],
-            :onboard_uuid => raw_info['results'][0]['onboard_uuid']
+          galvanize_id: raw_result['id'],
+          onboard_uuid: raw_result['onboard_uuid'],
+          email: raw_result['email'],
+          name: raw_result['name'],
+          first_name: raw_result['first_name'],
+          last_name: raw_result['last_name'],
+          about: raw_result['about'],
+          photo: raw_result['photo'],
+          home_location: raw_result['home_location'],
+          roles: raw_result['roles'],
+          cohort: raw_result['cohorts'], # TODO: deprecate this (without it, raw_result is a direct match)
+          cohorts: raw_result['cohorts'],
+          companies: raw_result['companies'],
         }
       end
 
       extra do
-        {
-            'raw_info' => raw_info
-        }
+        {'raw_info' => raw_info}
       end
 
       def raw_info
         @raw_info ||= access_token.get('/galvanize-api/v1/me?include_onboard=true').parsed
+      end
+
+      def raw_result
+        raw_info['results'][0]
       end
     end
   end
